22d05cce5d3929cd0930478a33114493489aad68
huliangjie <lin_032@hotmail.com>
2019-04-12 17:34:03 +0800

    connect

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include <ws2tcpip.h>
 #include <Windows.h>
 
-void init() {
+int init() {
 	WSADATA wsaData;
     WORD wVersionRequested = MAKEWORD(2, 0);
     int err = WSAStartup(wVersionRequested, &wsaData);
@@ -15,6 +15,7 @@ void init() {
         WSACleanup();
         return 0;
     }
+    return 1;
 }
 
 void deinit() {
@@ -22,13 +23,16 @@ void deinit() {
 }
 
 int main(int argc, char *argv[]) {
-    init();
+    if (!init()) {
+        return 0;
+    }
 
-    int i;
     const char *host = argv[1];
     const char *serv = NULL;
     struct addrinfo *resolved, *iter;
     struct addrinfo hints;
+    struct sockaddr *result = NULL;
+    size_t result_len = 0;
     memset(&hints, 0, sizeof(hints));
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_family = AF_UNSPEC;
@@ -36,40 +40,65 @@ int main(int argc, char *argv[]) {
     int res = getaddrinfo(host, NULL, &hints, &resolved);
     if (res) {
         printf("getaddrinfo = %d\n", res);
-        system("pause");
+        // system("pause");
         return 0;
     }
     char hbuf[NI_MAXHOST];
-    char sbuf[NI_MAXSERV];
 
-    for (i = 1, iter = resolved; iter; i++, iter = iter->ai_next) {
+    for (iter = resolved; iter; iter = iter->ai_next) {
         getnameinfo(iter->ai_addr, (socklen_t) iter->ai_addrlen,
             hbuf, (socklen_t) sizeof(hbuf),
             NULL, 0, 0);
 
         switch (iter->ai_family) {
-            case AF_INET: printf("[ipv4] %s\n", hbuf); break;
+            case AF_INET: printf("[ipv4] %s\n", hbuf); 
+                if (!result) {
+                    result = iter->ai_addr;
+                    result_len = iter->ai_addrlen;
+                }
+                break;
             case AF_INET6: printf("[ipv6] %s\n", hbuf); break;
             default: printf("[unkown] %s\n", hbuf); break;
         }
         // printf("resolve to %s\n", hbuf);
     }
+
+    if (result) {
+        SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        u_long argp = 0;
+        ioctlsocket(sock, FIONBIO, &argp);
+        struct sockaddr_in sa;
+        memcpy(&sa, result, sizeof(struct sockaddr_in));
+        sa.sin_port = htons(1234);
+
+        char sendbuf[1024];
+        int sendbufcap = 1024;
+        int sendbuflen = 0;
+        int sendcount = 0;
+
+        res = connect(sock, &sa, sizeof(struct sockaddr_in));
+        if (res < 0) {
+            printf("connect failed %d (%d)\n", res, WSAGetLastError());
+        } else {
+            while (1) {
+                sendbuflen = sprintf(sendbuf, "message#%d", sendcount++);
+                res = send(sock, sendbuf, sendbuflen, 0);
+                if (res <= 0) {
+                    printf("send failed %d (%d)\n", res, WSAGetLastError());
+                    break;
+                }
+                res = recv(sock, sendbuf, sendbufcap, 0);
+                if (res <= 0) {
+                    printf("recv failed %d (%d)\n", res, WSAGetLastError());
+                    break;
+                }
+                printf("echo %s\n", sendbuf);
+                Sleep(1000);
+            }
+        }
+        closesocket(sock);
+    }
     freeaddrinfo(resolved);
-
-    // SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	// u_long argp = 0;
-    // ioctlsocket(sock, FIONBIO, &argp);
-    // struct sockaddr_in sa;
-    // memset(&sa, 0, sizeof(sa));
-    // sa.sin_family = AF_INET;
-    // sa.sin_port = htons(6500);
-
-    // connect(sock, &sa, sizeof(sa));
-	// while (1) {
-	// 	// printf("loop\n");
-	// 	Sleep(1000);
-	// }
-	// closesocket(sock);
     deinit();
     system("pause");
     return 0;
