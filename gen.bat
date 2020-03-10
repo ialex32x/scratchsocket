4e6c9526bb03e67b792dc8422bacda05f21cd2b1
huliangjie <lin_032@hotmail.com>
2020-03-10 14:14:38 +0800

    t

--- gen.bat
+++ gen.bat
@@ -8,7 +8,8 @@ pushd scratchsocket
 rd /s /q x64
 mkdir x64
 pushd x64
-cmake -G "Visual Studio 15 2017 Win64" ..\..\..\
+REM cmake -G "Visual Studio 15 2017 Win64" ..\..\..\
+cmake -G "Visual Studio 16 2019" -A x64 ..\..\..\
 popd
 REM cmake --build x64 --config Debug
 popd
